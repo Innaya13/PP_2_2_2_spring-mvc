188308d2f9017371b71a8a1b0d024ab918275dba
mac <innakar@gmail.com>
2022-12-22 13:08:52 -0500

    fix

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -5,12 +5,12 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HelloController {
-
 	@GetMapping(value = "/")
 	public String printWelcome(ModelMap model) {
 		List<String> messages = new ArrayList<>();
@@ -20,5 +20,5 @@ public class HelloController {
 		model.addAttribute("messages", messages);
 		return "index";
 	}
-	
+
 }
